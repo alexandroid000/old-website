08c68935073ff16a2ccffc32563dd430965a72c9
Alli Nilles <anilles137@gmail.com>
2021-10-14 13:31:31 -0600

    publish

--- deploy.zsh
+++ deploy.zsh
@@ -1,21 +1,16 @@
-git stash
 git fetch --all
 git checkout master
+git pull
 stack exec myblog clean
 stack exec myblog build
 
-
 if [ $? = 0 ]; then
 
-    #cp -a _site/. .
+    #cp -a docs/. .
     git add -A
     git commit -m "publish"
     git push origin master:master
 
-    echo "MOVING BACK TO DEVELOP"
-    git checkout develop
-    git branch -D master
-    git stash pop
 else
     echo "Something went wrong"
 fi
